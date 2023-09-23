04899fc4913ca5814b0c661857c3915e7180d5b3
huawei <headcrabj1@gmail.com>
2023-09-23 10:29:13 +0500

    Make zerolog majestic again

--- main.go
+++ main.go
@@ -1,78 +1,14 @@
 package logger
 
 import (
-	"fmt"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"os"
 )
 
-type Logger struct {
-	core zerolog.Logger
-}
-
-func GetLogger() Logger {
-	return Logger{
-		log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: "02 Jan 15:04:05"}).With().Logger(),
-	}
-}
-
-func m(msg interface{}) string {
-	return fmt.Sprintf("%v", msg)
-}
-
-func (l *Logger) Panic(msg interface{}) {
-	l.core.Panic().Msg(m(msg))
-}
-
-func (l *Logger) Panicf(format string, v ...interface{}) {
-	l.core.Panic().Msgf(format, v...)
-}
-
-func (l *Logger) Fatal(msg interface{}) {
-	l.core.Fatal().Msg(m(msg))
-}
-
-func (l *Logger) Fatalf(format string, v ...interface{}) {
-	l.core.Fatal().Msgf(format, v...)
-}
-
-func (l *Logger) Error(msg interface{}) {
-	l.core.Error().Msg(m(msg))
-}
-
-func (l *Logger) Errorf(format string, v ...interface{}) {
-	l.core.Error().Msgf(format, v...)
-}
-
-func (l *Logger) Warn(msg interface{}) {
-	l.core.Warn().Msg(m(msg))
-}
-
-func (l *Logger) Warnf(format string, v ...interface{}) {
-	l.core.Warn().Msgf(format, v...)
-}
-
-func (l *Logger) Info(msg interface{}) {
-	l.core.Info().Msg(m(msg))
-}
-
-func (l *Logger) Infof(format string, v ...interface{}) {
-	l.core.Info().Msgf(format, v...)
-}
-
-func (l *Logger) Debug(msg interface{}) {
-	l.core.Debug().Msg(m(msg))
-}
-
-func (l *Logger) Debugf(format string, v ...interface{}) {
-	l.core.Debug().Msgf(format, v...)
-}
-
-func (l *Logger) Trace(msg interface{}) {
-	l.core.Trace().Msg(m(msg))
-}
-
-func (l *Logger) Tracef(format string, v ...interface{}) {
-	l.core.Trace().Msgf(format, v...)
+func Get() zerolog.Logger {
+	return log.Output(zerolog.ConsoleWriter{
+		Out:        os.Stdout,
+		TimeFormat: "02 Jan 15:04:05",
+	}).With().Caller().Logger()
 }
--- main_test.go
+++ main_test.go
@@ -1,63 +1,11 @@
 package logger
 
 import (
-	"errors"
 	"testing"
 )
 
-var newLogger = GetLogger()
-
-func TestPanic(t *testing.T) {
-	newLogger.Panic("Panic")
-}
-
-func TestFatal(t *testing.T) {
-	newLogger.Fatal("Fatal")
-}
+var newLogger = Get()
 
 func TestError(t *testing.T) {
-	newLogger.Error("Error")
-}
-
-func TestWarn(t *testing.T) {
-	newLogger.Warn("Warn")
-}
-
-func TestInfo(t *testing.T) {
-	newLogger.Info("Info")
-}
-
-func TestDebug(t *testing.T) {
-	newLogger.Debug("Debug")
-}
-
-func TestTrace(t *testing.T) {
-	newLogger.Trace("Trace")
-}
-
-func TestConverter(t *testing.T) {
-	newLogger.Info("String")
-	newLogger.Info("String " + "String2 " + "String3")
-	newLogger.Info(true)
-	newLogger.Info(100)
-	newLogger.Info([]int{1, 2, 3})
-	newLogger.Info([]string{"a", "b", "c"})
-	newLogger.Info(errors.New("new error"))
-	newLogger.Info(struct {
-		f1 string
-		f2 int
-	}{
-		f1: "String",
-		f2: 10,
-	})
-}
-
-func TestFormat(t *testing.T) {
-	//newLogger.Panicf("%d", 1)
-	//newLogger.Fatalf("%s", "str")
-	newLogger.Errorf("%f", 10.0)
-	newLogger.Warnf("%s", "str")
-	newLogger.Infof("%d", 10)
-	newLogger.Debugf("%t", true)
-	newLogger.Tracef("%p", []int{1, 2, 3})
+	newLogger.Error().Msg("New error message")
 }
