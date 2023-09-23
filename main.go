59818ca0d5466ead2de68cd2d2e2efb1be9ab414
huawei <headcrabj1@gmail.com>
2023-09-23 10:30:02 +0500

    Save in variable for reuse

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 	"os"
 )
 
+var l = log.Output(zerolog.ConsoleWriter{
+	Out:        os.Stdout,
+	TimeFormat: "02 Jan 15:04:05",
+}).With().Caller().Logger()
+
 func Get() zerolog.Logger {
-	return log.Output(zerolog.ConsoleWriter{
-		Out:        os.Stdout,
-		TimeFormat: "02 Jan 15:04:05",
-	}).With().Caller().Logger()
+	return l
 }
