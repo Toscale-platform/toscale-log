f7bb5e21ef1c53be751860f2978a2cd25da420d1
huawei <headcrabj1@gmail.com>
2023-09-26 00:02:26 +0500

    change caller from long to short

--- main.go
+++ main.go
@@ -4,12 +4,27 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"os"
+	"strconv"
 )
 
 var l = log.Output(zerolog.ConsoleWriter{
 	Out:        os.Stdout,
 	TimeFormat: "02 Jan 15:04:05",
 }).With().Caller().Logger()
+
+func init() {
+	zerolog.CallerMarshalFunc = func(pc uintptr, file string, line int) string {
+		short := file
+		for i := len(file) - 1; i > 0; i-- {
+			if file[i] == '/' {
+				short = file[i+1:]
+				break
+			}
+		}
+		file = short
+		return file + ":" + strconv.Itoa(line)
+	}
+}
 
 func Get() zerolog.Logger {
 	return l
